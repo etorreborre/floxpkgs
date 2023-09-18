43b494e1cf45c39c36849e260e49126e430d3c26
Matthew Kenigsberg <matthew@floxdev.com>
2023-09-18 10:32:14 -0700

    Update floxpkgs and drop deprecated boilerplate

--- flake.nix
+++ flake.nix
@@ -1,19 +1,5 @@
 {
-
-
-  # Declaration of external resources
-  # =================================
-
-  # =================================
-
-
-  description = "Floxpkgs/Project Template";
-  nixConfig.bash-prompt = "[flox] \\[\\033[38;5;172m\\]λ \\[\\033[0m\\]";
-
-  # Template DO NOT EDIT
-  # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  # TODO: injected by the cli, or used via registry?
   inputs.flox-floxpkgs.url = "github:flox/floxpkgs";
-  outputs = args @ {flox-floxpkgs, ...}: flox-floxpkgs.capacitor args (import ./flox.nix);
-  # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  outputs = args @ {flox-floxpkgs, ...}: flox-floxpkgs.project args (_: {});
 }
